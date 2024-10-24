adfb73b6141a4eee1c12ed3d6816e5404c30b8b0
syedm83 <syed_m8@yahoo.com>
2024-10-23 22:32:03 -0400

    grid + styling + user input right to row

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
+import Grid from './Grid'
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyup } = useWordle(solution)
+  const { currentGuess, handleKeyup, turn, guesses, isCorrect } = useWordle(solution)
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup) //tracks user input
@@ -10,8 +11,15 @@ export default function Wordle({ solution }) {
     return () => window.removeEventListener('keyup', handleKeyup)
   }, [handleKeyup]) //removes prev keyup so we dont have multiple
 
+  useEffect(() =>{
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
 
   return (
-    <div>current guess - {currentGuess}</div> //shows the user input on the screen
+    <div>
+    <div>solution - {solution}</div>
+    <div>current guess - {currentGuess}</div>
+    <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
+    </div>
   )
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,17 +3,67 @@ import { useState } from 'react'
 const useWordle = (solution) => {
   const [turn, setTurn] = useState(0) 
   const [currentGuess, setCurrentGuess] = useState('')
-  const [guesses, setGuesses] = useState([]) 
+  const [guesses, setGuesses] = useState([...Array(6)]) 
   const [history, setHistory] = useState([]) 
   const [isCorrect, setIsCorrect] = useState(false)
 
   const formatGuess = () => {
-    
+    let solutionArray = [...solution]
+    let formattedGuess = [...currentGuess].map((l) => {
+        return {key:l, color:'grey'}
+    })
+    //find any green colors now
+    formattedGuess.forEach((l,i) =>{
+        if (solutionArray[i] === l.key){
+            formattedGuess[i].color = 'green'
+            solutionArray[i] = null
+        }
+    })
+    //do it again for yellow
+    formattedGuess.forEach((l, i) => {
+        if (solutionArray.includes(l.key) && l.color !== 'green'){
+            //checks if its included anywhere
+            formattedGuess[i].color = 'yellow'
+            solutionArray[solutionArray.indexOf(l.key)] = null
+        }
+    })
+    return formattedGuess
   }
-  const addNewGuess = () => {
-
+  const addNewGuess = (formattedGuess) => {
+    if (currentGuess === solution) {
+      setIsCorrect(true)
+    }
+    setGuesses(prevGuesses => {
+      let newGuesses = [...prevGuesses]
+      newGuesses[turn] = formattedGuess
+      return newGuesses
+    })
+    setHistory(prevHistory => {
+      return [...prevHistory, currentGuess]
+    })
+    setTurn(prevTurn => {
+      return prevTurn + 1
+    })
+    setCurrentGuess('')
   }
   const handleKeyup = ({ key }) => {
+
+    if(key === 'Enter'){
+        if(turn>5){
+            console.log('all guesses have been used')
+            return
+        }
+        if(history.includes(currentGuess)){
+            console.log('you tried that already!')
+            return
+        }
+        if(currentGuess.length !== 5){
+            console.log('must be 5 chars')
+            return
+        }
+        const formatted = formatGuess()
+        addNewGuess(formatted)
+    }
 
     if(key === 'Backspace'){ //allows user to delete a character
         setCurrentGuess((prev) => {

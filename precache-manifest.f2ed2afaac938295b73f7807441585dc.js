d083a91ff1daa71c5dc5f4093eb287c6ce652fe5
syedm83 <syed_m8@yahoo.com>
2024-10-25 20:37:53 -0400

    Updates

---
+++ precache-manifest.f2ed2afaac938295b73f7807441585dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9517f17f41f3d731b60402b5f12fd7",
+    "url": "/wordle/index.html"
+  },
+  {
+    "revision": "908ec3240af551c13e0a",
+    "url": "/wordle/static/css/main.acb9088b.chunk.css"
+  },
+  {
+    "revision": "f77a592bd18fbcc527f1",
+    "url": "/wordle/static/js/2.a8f422eb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/wordle/static/js/2.a8f422eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de816d300c4b802c6f05",
+    "url": "/wordle/static/js/3.d9086783.chunk.js"
+  },
+  {
+    "revision": "908ec3240af551c13e0a",
+    "url": "/wordle/static/js/main.4cdce7dc.chunk.js"
+  },
+  {
+    "revision": "57d462fbf3a0d5e21f46",
+    "url": "/wordle/static/js/runtime-main.acf8a6e9.js"
+  }
+]);
